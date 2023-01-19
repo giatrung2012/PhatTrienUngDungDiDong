4050e53d34a7fdb40105f16b91c838d0817f5217
giatrung2012 <trunghsg2012@gmail.com>
2023-01-19 13:12:35 +0700

    Update

--- app/src/main/java/me/giatrung/baithuchanh/Bai3Activity.java
+++ app/src/main/java/me/giatrung/baithuchanh/Bai3Activity.java
@@ -12,8 +12,9 @@ public class Bai3Activity extends AppCompatActivity implements View.OnClickListe
 
     EditText etA, etB;
     TextView tvResult;
-    Button btnSubtract;
-    int a, b;
+    Button btnSubtract, btnMultiply, btnDivide, btnGCD, btnExit;
+    View.OnClickListener listener;
+    double a, b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +25,48 @@ public class Bai3Activity extends AppCompatActivity implements View.OnClickListe
         etB = (EditText) findViewById(R.id.et_b);
         tvResult = (TextView) findViewById(R.id.tv_result);
         btnSubtract = (Button) findViewById(R.id.btn_subtract);
+        btnMultiply = (Button) findViewById(R.id.btn_multiply);
+        btnDivide = (Button) findViewById(R.id.btn_divide);
+        btnGCD = (Button) findViewById(R.id.btn_gcd);
+        btnExit = (Button) findViewById(R.id.btn_exit);
 
-        btnSubtract.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = Double.parseDouble(etA.getText().toString());
+                b = Double.parseDouble(etB.getText().toString());
+                tvResult.setText(String.valueOf(a - b));
+            }
+        });
+
+        listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = Double.parseDouble(etA.getText().toString());
+                b = Double.parseDouble(etB.getText().toString());
+                tvResult.setText(String.valueOf(a / b));
+            }
+        };
+        btnDivide.setOnClickListener(listener);
+
+        btnGCD.setOnClickListener(new MyButtonClickListener(etA, etB, tvResult));
+
+//        btnExit.performClick() {
+//
+//        }
     }
 
     public void getSum(View view) {
-        a = Integer.parseInt(etA.getText().toString());
-        b = Integer.parseInt(etB.getText().toString());
+        a = Double.parseDouble(etA.getText().toString());
+        b = Double.parseDouble(etB.getText().toString());
         tvResult.setText(String.valueOf(a + b));
     }
 
     @Override
     public void onClick(View view) {
-        a = Integer.parseInt(etA.getText().toString());
-        b = Integer.parseInt(etB.getText().toString());
-        tvResult.setText(String.valueOf(a - b));
+        a = Double.parseDouble(etA.getText().toString());
+        b = Double.parseDouble(etB.getText().toString());
+        tvResult.setText(String.valueOf(a * b));
     }
 }
---
+++ app/src/main/java/me/giatrung/baithuchanh/MyButtonClickListener.java
@@ -0,0 +1,35 @@
+package me.giatrung.baithuchanh;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MyButtonClickListener implements View.OnClickListener {
+
+    EditText etA, etB;
+    TextView tvResult;
+    double a, b;
+
+    public MyButtonClickListener(EditText etA, EditText etB, TextView tvResult) {
+        this.etA = etA;
+        this.etB = etB;
+        this.tvResult = tvResult;
+    }
+
+    @Override
+    public void onClick(View view) {
+        a = Double.parseDouble(etA.getText().toString());
+        b = Double.parseDouble(etB.getText().toString());
+        tvResult.setText(String.valueOf(gcd(a, b)));
+    }
+
+    public double gcd(double a, double b) {
+        if (a < b) {
+            return gcd(b, a);
+        }
+        if (Math.abs(b) < 0.001) {
+            return a;
+        }
+        return (gcd(b, a - Math.floor(a / b) * b));
+    }
+}
